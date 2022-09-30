7ade3a0a862eb7cdffa7c50ce74dc38a18fb6151
Victor Neves <victorneves478@gmail.com>
2022-09-30 14:38:56 -0300

    Graças a deus
    kkkk, consegui arrumar o layout dos jogos. Falta agora ajustar as bordas do "pedra-papel-tesoura" e por último ajustar para que textos quebrem automaticamente

--- assets/js/mine.js
+++ assets/js/mine.js
@@ -1,18 +1,33 @@
 var grid = document.getElementById("mine-grid");
+var width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+var grid_size = width < "601" ? "5" : "10";
+var num_mines = grid_size === "10" ? "20" : "5";
 var game_active = true;
 // var testMode = false; //Turn this variable to true to see where the mines are
 
 const refresh = document.querySelector(".refresh");
 
 function generateGrid() {
-  // generate 10 by 10 grid
+  // generate grid_size by grid_size grid
   grid.innerHTML = "";
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < grid_size; i++) {
     row = grid.insertRow(i);
 
-    for (var j = 0; j < 10; j++) {
+    for (var j = 0; j < grid_size; j++) {
       cell = row.insertCell(j);
+      cell.addEventListener(
+        "contextmenu",
+        function (ev) {
+          ev.preventDefault();
+          rightClickCell(this);
+          return false;
+        },
+        false
+      );
       cell.addEventListener("click", function () {
         clickCell(this);
       });
@@ -27,9 +42,9 @@ function generateGrid() {
 
 function addMines() {
   // Add mines randomly
-  for (var i = 0; i < 20; i++) {
-    var row = Math.floor(Math.random() * 10);
-    var col = Math.floor(Math.random() * 10);
+  for (var i = 0; i < num_mines; i++) {
+    var row = Math.floor(Math.random() * grid_size);
+    var col = Math.floor(Math.random() * grid_size);
     var cell = grid.rows[row].cells[col];
     cell.setAttribute("data-mine", "true");
     // if (testMode) cell.innerHTML = "X";
@@ -38,8 +53,8 @@ function addMines() {
 
 function revealMines() {
   //Highlight all mines in red
-  for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
+  for (var i = 0; i < grid_size; i++) {
+    for (var j = 0; j < grid_size; j++) {
       var cell = grid.rows[i].cells[j];
       if (cell.getAttribute("data-mine") == "true") cell.className = "mine";
     }
@@ -50,8 +65,8 @@ function revealMines() {
 
 function checkLevelCompletion() {
   var levelComplete = true;
-  for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
+  for (var i = 0; i < grid_size; i++) {
+    for (var j = 0; j < grid_size; j++) {
       if (
         grid.rows[i].cells[j].getAttribute("data-mine") == "false" &&
         grid.rows[i].cells[j].innerHTML == ""
@@ -62,6 +77,10 @@ function checkLevelCompletion() {
   if (levelComplete) {
     revealMines();
   }
+}
+
+function rightClickCell(cell) {
+  cell.className = "right-clicked";
 }
 
 function clickCell(cell) {
@@ -75,10 +94,14 @@ function clickCell(cell) {
     var cellRow = cell.parentNode.rowIndex;
     var cellCol = cell.cellIndex;
     // alert(cellRow + " " + cellCol);
-    for (var i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
+    for (
+      var i = Math.max(cellRow - 1, 0);
+      i <= Math.min(cellRow + 1, grid_size - 1);
+      i++
+    ) {
       for (
         var j = Math.max(cellCol - 1, 0);
-        j <= Math.min(cellCol + 1, 9);
+        j <= Math.min(cellCol + 1, grid_size - 1);
         j++
       ) {
         if (grid.rows[i].cells[j].getAttribute("data-mine") == "true")
@@ -91,12 +114,12 @@ function clickCell(cell) {
       // Reveal all adjacent cells as they do not have a mine
       for (
         var i = Math.max(cellRow - 1, 0);
-        i <= Math.min(cellRow + 1, 9);
+        i <= Math.min(cellRow + 1, grid_size - 1);
         i++
       ) {
         for (
           var j = Math.max(cellCol - 1, 0);
-          j <= Math.min(cellCol + 1, 9);
+          j <= Math.min(cellCol + 1, grid_size - 1);
           j++
         ) {
           //Recursive Call
