fcd60da7e9c31f673c1a27d408779561145fd213
Victor Neves <victorneves478@gmail.com>
2022-10-20 10:53:15 -0300

    Fixing styling and it's ready to post

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -6,10 +6,6 @@ var swiper = new Swiper(".swiper-container", {
   freeMode: true,
   grabCursor: true,
   loop: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
