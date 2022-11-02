4493415b283b4913f8166e2198dd7c5f80801447
Victor Neves <victorneves478@gmail.com>
2022-11-02 12:17:37 -0300

    Created a games page

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -10,3 +10,19 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var swiper_cube = new Swiper(".swiper-cube", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+});
