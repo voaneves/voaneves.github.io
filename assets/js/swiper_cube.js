052c4e4cbe1811473da606220b03d786f6b1ac0b
Victor Neves <victorneves478@gmail.com>
2022-11-03 15:58:04 -0300

    Major changes, mainly the portfolio

--- assets/js/swiper_cube.js
+++ assets/js/swiper_cube.js
@@ -1,12 +1,15 @@
-// Swiper Configuration
 var swiper = new Swiper(".swiper-container", {
-  slidesPerView: "auto",
-  spaceBetween: 20,
-  centeredSlides: true,
+  effect: "cube",
   grabCursor: true,
-  loop: true,
+  cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  loop: true,
 });
