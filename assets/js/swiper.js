e9ffac3b303a0ea72ce4638b700decd044071f17
Victor Neves <victorneves478@gmail.com>
2022-11-05 01:11:01 -0300

    Changing Blog and Portfolio sections of the website

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,12 +1,5 @@
-// Swiper Configuration
 var swiper = new Swiper(".swiper-container", {
-  slidesPerView: "auto",
-  spaceBetween: 20,
-  centeredSlides: true,
-  grabCursor: true,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  spaceBetween: 100,
+  centeredSlides: true,
 });
