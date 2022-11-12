8bc45d4cd15518efea0b40bddd48f978ce34d24d
Victor Neves <victorneves478@gmail.com>
2022-11-12 18:17:22 -0300

    Huge changes, adding a Super Mario timeline and trying to customize the translate button to be more useful

---
+++ assets/js/timeline.js
@@ -0,0 +1,201 @@
+const timeline = [
+  {
+    year: 1994,
+    month_name: "Setembro 1994",
+    title: "Ano que nasci, em Araguaina/TO",
+  },
+  {
+    year: 2005,
+    month_name: "Janeiro 2005",
+    title: "Fiz o meu primeiro blog, Ponto Interativo",
+  },
+  {
+    year: 2011,
+    month_name: "Dezembro 2011",
+    title: "Terminei o ensino médio",
+  },
+  {
+    year: 2012,
+    month_name: "Janeiro 2012",
+    title: "Entrei na UNICAMP",
+  },
+  {
+    year: 2012,
+    month_name: "Dezembro 2012",
+    title: "Minha iniciação científica em nanotubos de titânio",
+  },
+  {
+    year: 2014,
+    month_name: "Julho 2014",
+    title: "Entre na empresa Jr. MTE, na UNICAMP",
+  },
+  {
+    year: 1992,
+    month_name: "October 1992",
+    title: "Super Mario Land: 6 Golden Coins",
+  },
+  {
+    year: 1995,
+    month_name: "August 1995",
+    title: "Super Mario World 2: Yoshi's Island",
+  },
+  { year: 1996, month_name: "June 1996", title: "Super Mario 64" },
+  {
+    year: 2002,
+    month_name: "July 2002",
+    title: "Super Mario Sunshine",
+  },
+  {
+    year: 2006,
+    month_name: "May 2006",
+    title: "New Super Mario Bros.",
+  },
+  {
+    year: 2007,
+    month_name: "November 2007",
+    title: "Super Mario Galaxy",
+  },
+  {
+    year: 2009,
+    month_name: "November 2009",
+    title: "New Super Mario Bros. Wii",
+  },
+  {
+    year: 2010,
+    month_name: "May 2010",
+    title: "Super Mario Galaxy 2",
+  },
+];
+
+const mario = document.getElementById("mario");
+const ground = document.getElementById("ground");
+const grass = document.getElementById("grass");
+const eventsContainer = document.getElementById("events");
+let currentIndex = -1;
+let currentPipe;
+let int1;
+
+// click handler
+const pipeHandler = (event) => {
+  clearInterval(int1);
+  document.getElementById("info").style.display = "none";
+
+  // clear old
+  !currentPipe || currentPipe.classList.remove("active");
+
+  // get index
+  const index = parseInt(event.currentTarget.dataset.index);
+
+  // walk
+  const xpos = -100 - index * 150 - 25;
+  const curXpos = -100 - currentIndex * 150 - 25;
+  const distance = curXpos - xpos;
+  const duration = Math.abs(distance) * 3;
+  // console.log(distance);
+  eventsContainer.style.transitionDuration = `${duration}ms`;
+  eventsContainer.style.transform = `translateX(${xpos}px)`;
+  ground.style.transitionDuration = `${duration}ms`;
+  ground.style.backgroundPosition = `${xpos}px 32px`;
+  grass.style.transitionDuration = `${duration}ms`;
+  grass.style.backgroundPosition = `${xpos}px 0`;
+
+  // walk style
+  const dir = distance < 0 ? "left" : "right";
+  mario.classList.remove(
+    "idle",
+    "walk-left",
+    "walk-right",
+    "search-left",
+    "search-right"
+  );
+  mario.classList.add(`walk-${dir}`);
+  int1 = setTimeout(
+    (dir, target) => {
+      mario.classList.remove(`walk-${dir}`);
+      mario.classList.add(`search-${dir}`);
+      target.classList.add("active");
+      playSfx("pipe");
+    },
+    duration,
+    dir,
+    event.currentTarget
+  );
+
+  // store position
+  currentIndex = index;
+  currentPipe = event.currentTarget;
+};
+
+// setup timeline
+timeline.forEach((event, index) => {
+  const e = document.createElement("div");
+  e.classList.add("event");
+  e.dataset.index = index;
+  e.dataset.title = event.title;
+  e.dataset.month = event.month_name;
+  eventsContainer.appendChild(e);
+  e.addEventListener("click", pipeHandler.bind(this));
+});
+/* document.addEventListener("keydown", function (e) {
+  e = e || window.event;
+
+  if (e.key === "ArrowLeft") {
+    mario.classList.add("walk-left");
+  } else if (e.key === "ArrowRight") {
+    mario.classList.add("walk-right");
+  }
+});
+
+document.addEventListener("keyup", function (e) {
+  e = e || window.event;
+
+  if (e.key === "ArrowLeft") {
+    mario.classList.remove("walk-left");
+    mario.classList.add("idle");
+  } else if (e.key === "ArrowRight") {
+    mario.classList.remove("walk-right");
+    mario.classList.add("idle");
+  }
+}); */
+
+/* Audio handling */
+const canAudio = "AudioContext" in window || "webkitAudioContext" in window;
+const buffers = {};
+let context = void 0;
+
+if (canAudio) {
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext(); // Make it crossbrowser
+  var gainNode = context.createGain();
+  gainNode.gain.value = 1; // set volume to 100%
+}
+
+const playSfx = function play(id) {
+  if (!canAudio || !buffers.hasOwnProperty(id)) return;
+  const buffer = buffers[id];
+  const source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start();
+};
+
+const loadBuffers = (urls, ids) => {
+  if (typeof urls == "string") urls = [urls];
+  if (typeof ids == "string") ids = [ids];
+  urls.forEach((url, index) => {
+    window
+      .fetch(url)
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) =>
+        context.decodeAudioData(
+          arrayBuffer,
+          (audioBuffer) => {
+            buffers[ids[index]] = audioBuffer;
+          },
+          (error) => console.log(error)
+        )
+      );
+  });
+};
+
+loadBuffers(["https://assets.codepen.io/439000/smb_pipe.mp3"], ["pipe"]);
