b7b2bf7a485633e816fc1019b086557315d96843
Victor Neves <victorneves478@gmail.com>
2022-11-19 21:44:17 -0300

    Revert some changes, but create a loadCSS.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
       document.querySelector(".active-btn").classList.remove("active-btn");
-      loadCSS(button.dataset.id);
       this.classList.add("active-btn");
       document.querySelector(".active").classList.remove("active");
       document.getElementById(button.dataset.id).classList.add("active");
@@ -38,26 +37,3 @@ var height =
   window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
-
-var filesAdded = 'home';
-
-// To load CSS file
-function loadCSS(fileName) { 
-  console.log(fileName);
-    if(filesAdded.indexOf(fileName) !== -1)
-        return;
-  
-    var head = document.getElementsByTagName('head')[0];
-      
-    // Creating link element
-    var style = document.createElement('link'); 
-    style.href = '/assets/styles/' + fileName + '.css';
-    style.type = 'text/css';
-    style.rel = 'stylesheet';
-    head.append(style);
-
-    console.log('/assets/styles/' + fileName + '.css');
-      
-    // Adding the name of the file to keep record
-    filesAdded += fileName;
-}
