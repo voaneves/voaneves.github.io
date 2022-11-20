001ad69796447f30d511993f477d9ebcc6ad9bb2
Victor Neves <victorneves478@gmail.com>
2022-11-19 21:29:15 -0300

    Testing new file load implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,19 @@
   [...document.querySelectorAll(".control")].forEach((button) => {
     button.addEventListener("click", function () {
       document.querySelector(".active-btn").classList.remove("active-btn");
+      loadCSS(button.dataset.id);
       this.classList.add("active-btn");
       document.querySelector(".active").classList.remove("active");
       document.getElementById(button.dataset.id).classList.add("active");
-
-      window.onscroll = function () {
-        var scrolled = window.pageYOffset || document.documentElement.scrollTop,
-          arrowUp = document.querySelector(".arrow-up");
-
-        arrowUp.style.opacity = scrolled > 150 ? 1 : 0;
-      };
     });
   });
+
+  window.onscroll = function () {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop,
+    arrowUp = document.querySelector(".arrow-up");
+
+    arrowUp.style.opacity = scrolled > 150 ? 1 : 0;
+  };
 
   document.querySelector(".sidebar-toggle").addEventListener("click", () => {
     const sidebar = document.querySelector(".sidebar");
@@ -37,3 +38,26 @@ var height =
   window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
+
+var filesAdded = 'home';
+
+// To load CSS file
+function loadCSS(fileName) { 
+  console.log(fileName);
+    if(filesAdded.indexOf(fileName) !== -1)
+        return;
+  
+    var head = document.getElementsByTagName('head')[0];
+      
+    // Creating link element
+    var style = document.createElement('link'); 
+    style.href = '/assets/styles/' + fileName + '.css';
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    head.append(style);
+
+    console.log('/assets/styles/' + fileName + '.css');
+      
+    // Adding the name of the file to keep record
+    filesAdded += fileName;
+}
