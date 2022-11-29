8149e6845b38bdcb7fee5278f727630c8d619d48
Victor Neves <victorneves478@gmail.com>
2022-11-28 21:02:12 -0300

    Last fixes of today

--- worker.js
+++ worker.js
@@ -20,7 +20,6 @@ const RUNTIME = "runtime";
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   "./", // Alias for index.html
-  "en.html",
 ];
 
 // The install handler takes care of precaching the resources we always need.
