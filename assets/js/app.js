f40892903708482a013dcfa24a5b5e888137f6cc
Victor Neves <victorneves478@gmail.com>
2022-12-02 21:22:45 -0300

    Updating items and menu with intersection observer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,52 @@
 (function () {
-  [...document.querySelectorAll(".control")].forEach((button) => {
-    button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
+  /* Menu Handler */
+  const controls = document.querySelectorAll(".control");
+  const sections = document.querySelectorAll("section");
+  const observer_controls = new IntersectionObserver(
+    (entries, observer_controls) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = Array.from(sections).indexOf(entry.target);
+          console.log(entry.target);
+          document.querySelector(".active-btn").classList.remove("active-btn");
+          controls[index].classList.add("active-btn");
+        }
+      });
+    },
+    {
+      threshold: 0.05,
+    }
+  );
+  sections.forEach((section) => {
+    observer_controls.observe(section);
+  });
+  const sidebar = document.querySelector(".sidebar");
+  controls.forEach((control) => {
+    control.addEventListener("click", function () {
       sidebar.classList.toggle("open");
     });
   });
+  document.querySelector(".sidebar-toggle").addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+  });
 
+  /* AOC Handler */
+  const scroll_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  });
+  document
+    .querySelectorAll(".hidden")
+    .forEach((el) => scroll_observer.observe(el));
+
+  /* Scroll-to-top handler */
   window.onscroll = function () {
     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
     document.querySelector(".arrow-up").style.opacity = scrolled > 150 ? 1 : 0;
   };
 
-  document.querySelector(".sidebar-toggle").addEventListener("click", () => {
-    document.querySelector(".sidebar").classList.toggle("open");
-  });
-
+  /* Theme handler */
   document.getElementById("theme").addEventListener("click", function () {
     if (darkTheme.matches) document.body.classList.toggle("light-mode");
     else document.body.classList.toggle("dark-mode");
--- assets/js/scroll_observer.js
+++
@@ -1,8 +0,0 @@
-const scroll_observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle("show", entry.isIntersecting);
-  });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => scroll_observer.observe(el));
