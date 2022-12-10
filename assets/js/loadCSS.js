71002078c54e217d05b143d393f31770b414a0ed
Victor Neves <victorneves478@gmail.com>
2022-12-10 01:29:10 -0300

    The minify on vscode is was not working properly, fixing it

--- assets/js/loadCSS.js
+++ assets/js/loadCSS.js
@@ -2,22 +2,21 @@
         loadCSS(button.dataset.id); 
 */
 
-var filesAdded = '';
+var filesAdded = "";
 
 // To load CSS file
-function loadCSS(fileName) { 
-    if(filesAdded.indexOf(fileName) !== -1)
-        return;
-  
-    var head = document.getElementsByTagName('head')[0];
-      
-    // Creating link element
-    var style = document.createElement('link'); 
-    style.href = '/assets/styles/' + fileName + '.css';
-    style.type = 'text/css';
-    style.rel = 'stylesheet';
-    head.append(style);
-      
-    // Adding the name of the file to keep record
-    filesAdded += fileName;
+function loadCSS(fileName) {
+  if (filesAdded.indexOf(fileName) !== -1) return;
+
+  var head = document.getElementsByTagName("head")[0];
+
+  // Creating link element
+  var style = document.createElement("link");
+  style.href = "/assets/styles/" + fileName + ".css";
+  style.type = "text/css";
+  style.rel = "stylesheet";
+  head.append(style);
+
+  // Adding the name of the file to keep record
+  filesAdded += fileName;
 }
